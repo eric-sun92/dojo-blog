4c69fbf01dd43fd73a68638c9b288c16a21426f4
Eric <eric.sun.ecs92@yale.edu>
2022-07-30 23:20:02 -0400

    reactIntro-finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -13,8 +14,9 @@ function App() {
         <div className="content">
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route path="/create" element={<Create />} />
+            <Route exact path="/create" element={<Create />} />
             <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
